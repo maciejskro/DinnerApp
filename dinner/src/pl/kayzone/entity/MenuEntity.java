286359c22eed02207f704ffe0491a498253dfc3e
maciejskro <maciejskro@gmail.com>
2017-11-13 00:16:41 +0100

    Signed-off-by: maciejskro <maciejskro@gmail.com>

--- dinner/src/pl/kayzone/entity/MenuEntity.java
+++ dinner/src/pl/kayzone/entity/MenuEntity.java
@@ -8,13 +8,47 @@ import org.mongodb.morphia.annotations.Entity;
 @Entity
 class MenuEntity {
 
+	private ObjectId id;
+	private Integer weekid;
+	private String dayname;
+	private List<String> itemmenu;
+	
 	public MenuEntity() {
 		
-		ObjectId id;
-		Integer weekid;
-		String dayname;
-		List<String> itemmenu;
 		
+		
+	}
+
+	ObjectId getId() {
+		return id;
+	}
+
+	void setId(ObjectId id) {
+		this.id = id;
+	}
+
+	Integer getWeekid() {
+		return weekid;
+	}
+
+	void setWeekid(Integer weekid) {
+		this.weekid = weekid;
+	}
+
+	String getDayname() {
+		return dayname;
+	}
+
+	void setDayname(String dayname) {
+		this.dayname = dayname;
+	}
+
+	List<String> getItemmenu() {
+		return itemmenu;
+	}
+
+	void setItemmenu(List<String> itemmenu) {
+		this.itemmenu = itemmenu;
 	}
 
 }
