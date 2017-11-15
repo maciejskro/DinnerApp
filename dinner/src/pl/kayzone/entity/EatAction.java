171e9b8a9ada597b41ccdf69e173c1d6808663e6
maciejskro <maciejskro@gmail.com>
2017-11-15 22:12:27 +0100

    new add xhtml

--- dinner/src/pl/kayzone/entity/EatAction.java
+++ dinner/src/pl/kayzone/entity/EatAction.java
@@ -1,9 +1,37 @@
 package pl.kayzone.entity;
 
+import java.util.Date;
+
+import org.bson.types.ObjectId;
+import org.mongodb.morphia.annotations.Entity;
+import org.mongodb.morphia.annotations.Field;
+import org.mongodb.morphia.annotations.Id;
+import org.mongodb.morphia.annotations.Index;
+import org.mongodb.morphia.annotations.Indexes;
+import org.mongodb.morphia.annotations.Reference;
+
+@Entity ("eating")
+@Indexes ( @Index(value="customer", fields = @Field("customer")))
 class EatAction {
+	
+	@Id
+	private ObjectId id;
+	@Reference 
+	private Customer customer;
+	private Date eatingdate;
+	@Reference 
+	private  MenuEntity menu;
 
 	public EatAction() {
-		// TODO Auto-generated constructor stub
+		
+	}
+
+	public Date getEatingdate() {
+		return eatingdate;
+	}
+
+	public void setEatingdate(Date eatingdate) {
+		this.eatingdate = eatingdate;
 	}
 
 }
