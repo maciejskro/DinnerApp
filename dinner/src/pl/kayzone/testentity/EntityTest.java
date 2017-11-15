0ddbe27577aec6b50a6b6276502d5528a67a504c
maciejskro <maciejskro@gmail.com>
2017-11-15 23:44:50 +0100

    update pom

---
+++ dinner/src/pl/kayzone/testentity/EntityTest.java
@@ -0,0 +1,48 @@
+/**
+ * 
+ */
+package pl.kayzone.testentity;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author smaciej
+ *
+ */
+class EntityTest {
+
+	/**
+	 * Test method for {@link pl.kayzone.entity.Customer#setId(org.bson.types.ObjectId)}.
+	 */
+	@Test
+	void testSetId() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link pl.kayzone.entity.Customer#setCustomerName(java.lang.String)}.
+	 */
+	@Test
+	void testSetCustomerName() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link pl.kayzone.entity.Customer#setCustomerSurname(java.lang.String)}.
+	 */
+	@Test
+	void testSetCustomerSurname() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link pl.kayzone.entity.Customer#setCustomerPass(java.lang.String)}.
+	 */
+	@Test
+	void testSetCustomerPass() {
+		fail("Not yet implemented");
+	}
+
+}
